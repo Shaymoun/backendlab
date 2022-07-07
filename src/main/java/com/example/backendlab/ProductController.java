dd59007d25efd17fa092328b68ee6e8100d922b5
Shaymoun <enter312123@gmail.com>
2022-07-07 19:46:30 +0200

    Added editing Product method.

--- src/main/java/com/example/backendlab/ProductController.java
+++ src/main/java/com/example/backendlab/ProductController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -28,6 +29,23 @@ public class ProductController {
     @PostMapping("/products/save")
     public String saveProduct(Product product){
         productRepo.save(product);
-        return "redirect:/";
+        return "redirect:/products";
+    }
+
+    @GetMapping("/products")
+    public String listProducts(Model model){
+        List<Product> listProducts = productRepo.findAll();
+        model.addAttribute("listProducts",listProducts);
+        return "products";
+    }
+
+    @GetMapping("/products/edit/{id}")
+    public String showEditProductForm(@PathVariable("id") Integer id, Model model){
+        Product product = productRepo.findById(id).get();
+        model.addAttribute("product",product);
+
+        List<Category> listCategories = categoryRepo.findAll();
+        model.addAttribute("listCategories", listCategories);
+        return "product_form";
     }
 }
