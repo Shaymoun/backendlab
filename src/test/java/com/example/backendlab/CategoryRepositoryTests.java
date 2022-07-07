625d0b3f4bdfd1e0a73695f5420e0cae50eb9c39
Shaymoun <enter312123@gmail.com>
2022-07-07 16:42:38 +0200

    Created Categories view.

---
+++ src/test/java/com/example/backendlab/CategoryRepositoryTests.java
@@ -0,0 +1,24 @@
+package com.example.backendlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class CategoryRepositoryTests {
+    @Autowired
+    private CategoryRepository repo;
+
+    @Test
+    public void testCreateCategory(){
+        Category savedCategory = repo.save(new Category("Electronics"));
+
+        assertThat(savedCategory.getId()).isGreaterThan(0);
+    }
+}
